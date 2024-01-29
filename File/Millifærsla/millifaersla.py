442462664cee394de9030a69631ebdd05351024c
mohaMgT <157659653+mohaMgT@users.noreply.github.com>
2024-01-29 17:46:57 +0330

    Add files via upload

---
+++ File/Millifærsla/millifaersla.py
@@ -0,0 +1,10 @@
+lst = []
+for i in range(0,3):
+    lst.append(int(input()))
+min_val = min(lst)
+if min_val == lst[0]:
+    print("Monnei")
+elif min_val == lst[1]:
+    print("Fjee")
+else :
+    print("Dolladollabilljoll")
